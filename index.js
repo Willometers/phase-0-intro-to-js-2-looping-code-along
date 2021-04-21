b4139d1817115a5229371f977ec02ee13fbd51b7
BillyMiles <williammiles201@gmail.com>
2021-04-20 20:07:43 -0400

    AC

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     // debugger;
+//   }
+
+//   return gifts;
+// }
+
+function writeCards( names, event ) {
+  let theCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    theCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
   }
-
-  return gifts;
+  return theCards
 }
+
+function
