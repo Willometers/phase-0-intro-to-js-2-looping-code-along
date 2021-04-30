e2cdc7765b18cb8bbedffa14c505e6bfd3b24e62
BillyMiles <williammiles201@gmail.com>
2021-04-30 15:12:43 -0400

    commit tree 309e735683a463ab99b3ab0be38a46433b132750

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// const gifts = ["teddy bear", "drone", "doll"];
-
-// function wrapGifts(gifts) {
-//   for (let i = 0; i < gifts.length; i++) {
-//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     // debugger;
-//   }
-
-//   return gifts;
-// }
-
-function writeCards( names, event ) {
-  let theCards = []
-  for ( let i = 0; i < names.length; i++ ) {
-    theCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+function writeCards(name, event) {
+  let thankYouCards = []
+  for (let i = 0; i < name.length; i++) {
+    thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
   }
-  return theCards
+  return thankYouCards;
 }
 
-function
+function countDown(num) {
+  while (num > 0) {
+    console.log(num);
+    num -=1;
+  }
+  console.log(num);
+}
